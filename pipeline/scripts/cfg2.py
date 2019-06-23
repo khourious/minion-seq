f2cae0f5fcb1ac84db9d1446be96b66ebc7496e9
lpmor22 <laisepaixao@live.com>
2019-06-23 14:31:43 -0300

    minor changes
    2019-06-23

--- pipeline/scripts/cfg2.py
+++ pipeline/scripts/cfg2.py
@@ -1,5 +1,5 @@
 config = { 'ref_genome':
-                '/home/laisepaixao/softwares/minionSeq/pipeline/refs/ref_zika_KJ776791.fasta',
+                '~/softwares/minionSeq/pipeline/refs/ref_zika_KJ776791.fasta',
            'primer_scheme':
-                '/home/laisepaixao/softwares/minionSeq/pipeline/metadata/ZIKA_v2_500.amplicons.ver2.bed'
+                '~/softwares/minionSeq/pipeline/metadata/ZIKA_v2_500.amplicons.ver2.bed'
         }
