b5aeff44a5b91753787c077fff45646665404cfe
Laise de Moraes <laisepaixao@live.com>
2019-08-28 07:53:44 -0300

    20190828

--- INSTALL.sh
+++ INSTALL.sh
@@ -7,8 +7,9 @@ then
   echo "Downloading Guppy..."
   curl https://mirror.oxfordnanoportal.com/software/analysis/ont-guppy-cpu_3.2.2_linux64.tar.gz --output $HOME/softwares/ont-guppy-cpu.tar.gz
   echo "Installing Guppy..."
-  cd $HOME/softwares/ && tar -vzxf ont-guppy-cpu.tar.gz && rm -rf ont-guppy-cpu.tar.gz
-  echo "export PATH="$HOME/softwares/ont-guppy-cpu/bin:/usr/local/share/rsi/idl/bin:$PATH"" >> $HOME/.bashrc
+  cd $HOME/softwares/ && tar -vzxf ont-guppy-cpu.tar.gz && mv ont-guppy-cpu/ guppy/ && rm -rf ont-guppy-cpu.tar.gz
+  export PATH="$HOME/softwares/guppy/bin:/usr/local/share/rsi/idl/bin:$PATH"
+  echo 'export PATH="$HOME/softwares/guppy/bin:/usr/local/share/rsi/idl/bin:$PATH"' >> $HOME/.bashrc
   echo "Successfully installed Guppy!" && echo ""
 else
   echo "Guppy already installed!" && echo ""
@@ -19,23 +20,23 @@ then
   echo "Downloading Miniconda..."
   curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh --output $HOME/softwares/miniconda.sh
   echo "Installing Miniconda..."
-  bash $HOME/softwares/miniconda.sh -bfp $HOME/softwares/miniconda && rm --recursive --force $HOME/softwares/miniconda.sh
-  echo "export PATH="$HOME/softwares/miniconda/bin:/usr/local/share/rsi/idl/bin:$PATH""
-  echo "export PATH="$HOME/softwares/miniconda/bin:/usr/local/share/rsi/idl/bin:$PATH"" >> $HOME/.bashrc
+  bash $HOME/softwares/miniconda.sh -bfp $HOME/softwares/conda && rm --recursive --force $HOME/softwares/miniconda.sh
+  export PATH="$HOME/softwares/conda/bin:/usr/local/share/rsi/idl/bin:$PATH"
+  echo 'export PATH="$HOME/softwares/conda/bin:/usr/local/share/rsi/idl/bin:$PATH"' >> $HOME/.bashrc
   echo "Successfully installed Miniconda!" && echo ""
 else
   echo "Miniconda already installed!" && echo ""
 fi
-# Update conda
+# Updating conda
 echo "Updating conda..."
-conda update --name base --channel defaults conda --yes
+conda update --yes --name base --channel defaults conda
 # Create conda environment: minion-seq
 echo "Creating "minion-seq" environment..."
-conda create --name minion-seq --yes
+conda create --yes --name minion-seq
 echo "Installing packages in minion-seq..."
-conda install --name minion-seq --channel anaconda biopython drmaa pycoqc --yes
-conda install --name minion-seq --channel bioconda bwa fastx-toolkit nanopolish porechop pysam r-minionqc snakemake samtools vcftools --yes
-conda install --name minion-seq --channel conda-forge boto boto3 ipython libiconv ncurses numpy pandas psutil python pyvcf r-optparse --yes
-conda install --name minion-seq --channel omgarcia gcc-6 --yes
-conda install --name minion-seq --channel r r r-reshape r-reshape2 --yes
+conda install --yes --name minion-seq --channel anaconda biopython drmaa pycoqc
+conda install --yes --name minion-seq --channel biobuilds fastx-toolkit
+conda install --yes --name minion-seq --channel bioconda bwa nanopolish porechop pysam r-minionqc snakemake samtools vcftools
+conda install --yes --name minion-seq --channel conda-forge boto boto3 ipython libiconv ncurses numpy pandas psutil python pyvcf r-optparse
+conda install --yes --name minion-seq --channel r r r-reshape r-reshape2
 echo "" && echo "Done!"
